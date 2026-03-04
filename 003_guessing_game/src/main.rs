6b6693ceda2b5d214bd8f28fad4821406f31869f
Jerowe Tan <j.tan@toyota-mobilitysolutions.ph>
2026-03-04 16:52:31 +0800

    chore: added new knowledge and complete the guessing game

--- 003_guessing_game/src/main.rs
+++ 003_guessing_game/src/main.rs
@@ -1,7 +1,11 @@
+use std::cmp::Ordering;
 use std::io;
+
+use rand::Rng;
 
 fn main() {
     println!("Gues the number (UwU)!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -11,4 +15,11 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
