2004b666c15997783ded414aae7d2cf30c1a070c
Jerowe Tan <j.tan@toyota-mobilitysolutions.ph>
2026-03-06 11:28:40 +0800

    feat: finish guessing game

--- 003_guessing_game/src/main.rs
+++ 003_guessing_game/src/main.rs
@@ -1,25 +1,43 @@
 use std::cmp::Ordering;
 use std::io;
+use std::io::Write;
 
 use rand::Rng;
 
 fn main() {
-    println!("Gues the number (UwU)!");
+    println!("Guess the number (UwU)!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("Please input your guess.");
+    loop {
+        print!("Please input your guess: ");
+        io::stdout().flush().expect("Failed to flush stdout");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        if guess.trim().to_lowercase() == "quit" {
+            println!("Goodbye! 👋");
+            break;
+        }
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        println!("You guessed: {guess}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win! Congratulations! 🎉");
+                break;
+            }
+        }
     }
 }
